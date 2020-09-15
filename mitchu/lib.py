4f6af91c37dede71cba3eddf611e0b5d68a6aad8
Alec McGail <amcgail2@gmail.com>
2020-09-15 12:56:39 -0400

    ehh

--- mitchu/lib.py
+++ mitchu/lib.py
@@ -25,12 +25,36 @@ class Metric:
         self.snaps = {}
 
 class Meeting:
+    """
+    
+    Args:
+        ppl: The people present in this meeting
+        resources: I honestly don't know
+
+    Attributes:
+        ppl: The people present in this meeting
+        resources: I honestly don't know
+    """
     def __init__(self,ppl=[],resources=0):
         self.ppl = ppl
         self.resources = resources
     
 
 class Person:
+    """
+    
+    Args:
+        **kwargs: The people present in this meeting
+
+    Attributes:
+        actions: the actions within this person's repertoire
+        args: 
+            Holds all other arguments from instantiating the person. 
+            Thus we can give a person some attribute by simply instantiating them as Person(color='red')
+        name:
+            Randomly generated female name for this person
+        
+    """
     names_already = set()
     
     def __init__(self, **kwargs):
@@ -57,8 +81,15 @@ class Person:
     def __repr__(self):
         return "[%s]"%self.name
     
-    def log(self, message, context):
-        context.log( "[%s]: %s" % (self.name, message))
+    def log(self, message, simulation):
+        """
+        Logs a message with this person's name
+
+        Args:
+            message: what you want to log
+            simulation: the simulation currently running
+        """
+        simulation.log( "[%s]: %s" % (self.name, message))
             
             
 class Action:
@@ -83,8 +114,15 @@ class Action:
     def ct(self):
         self._next_t = None
         
-    def log(self, message, context):
-        context.log( "[%s][%s]: %s" % (self.person.name, str(self.__class__.__name__), message))
+    def log(self, message, simulation):
+        """
+        Logs a message with the person and action name
+
+        Args:
+            message: what you want to log
+            simulation: the simulation currently running
+        """
+        simulation.log( "[%s][%s]: %s" % (self.person.name, str(self.__class__.__name__), message))
         
         
         
@@ -108,6 +146,20 @@ class sim:
             self.metrics[met.__name__] = mtoadd
     
     def run(self, MAX_T=100, INC_T=0.5):
+        """
+        this is the main function to run a simulation.
+        The function iterates from 0 to `MAX_T`, with step size `INC_T`.
+        In each iteration, 
+        
+        1. all metrics are measured
+        2. all individuals' propensity to action are reevaluated
+        3. based on this propensity, the next time to action is sampled from an exponential distribution
+        4. these events are ordered, and executed in order
+
+        Parameters:
+            MAX_T: the time at which to end the simulation
+            INC_T: the increment for updating propensities
+        """
         
         for base_t in np.arange(0, MAX_T, INC_T):
             
@@ -150,9 +202,61 @@ class sim:
                     
                     
     def log(self, message):
+        """
+        Logs a message with the time, conditional on `self.debug`
+
+        Args:
+            message: what you want to log
+        """
         if self.debug:
             print("(%s) %s" % (self.CURRENT_WORLD_TIME, message))
         pass
     
     
-    
+
+
+
+"""
+exponential(scale=1.0, size=None)
+
+Draw samples from an exponential distribution.
+
+Its probability density function is
+
+.. math:: f(x; \\frac{1}{\\beta}) = \\frac{1}{\\beta} \\exp(-\\frac{x}{\\beta}),
+
+for ``x > 0`` and 0 elsewhere. :math:`\\beta` is the scale parameter,
+which is the inverse of the rate parameter :math:`\\lambda = 1/\\beta`.
+The rate parameter is an alternative, widely used parameterization
+of the exponential distribution [3]_.
+
+The exponential distribution is a continuous analogue of the
+geometric distribution.  It describes many common situations, such as
+the size of raindrops measured over many rainstorms [1]_, or the time
+between page requests to Wikipedia [2]_.
+
+Parameters
+----------
+scale : float or array_like of floats
+    The scale parameter, :math:`\\beta = 1/\\lambda`. Must be
+    non-negative.
+size : int or tuple of ints, optional
+    Output shape.  If the given shape is, e.g., ``(m, n, k)``, then
+    ``m * n * k`` samples are drawn.  If size is ``None`` (default),
+    a single value is returned if ``scale`` is a scalar.  Otherwise,
+    ``np.array(scale).size`` samples are drawn.
+
+Returns
+-------
+out : ndarray or scalar
+    Drawn samples from the parameterized exponential distribution.
+
+References
+----------
+.. [1] Peyton Z. Peebles Jr., "Probability, Random Variables and
+        Random Signal Principles", 4th ed, 2001, p. 57.
+.. [2] Wikipedia, "Poisson process",
+        https://en.wikipedia.org/wiki/Poisson_process
+.. [3] Wikipedia, "Exponential distribution",
+        https://en.wikipedia.org/wiki/Exponential_distribution
+"""
